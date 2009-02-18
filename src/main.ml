bcffbcea4d16ee00da5e4a9e6774dc2f46b1a34d
afu <afu@71c2348b-e008-4d38-be3d-d357f6eb5cc4>
2009-02-18 21:09:11 +0000

    We are actually emitting code :-)
    git-svn-id: https://svn.fugmann.net/borderline@20 71c2348b-e008-4d38-be3d-d357f6eb5cc4

--- src/main.ml
+++ src/main.ml
@@ -1,9 +1,12 @@
 open Parse
 open Frontend
 open Zone
+open Emit
 open Printf
  
 let _ = 
   let nodes = parse "test.bl" in
   let zones = List.filter ( function node -> match node with Zone(_,_) -> true | _ -> false ) nodes in
-    List.map pretty_print zones; 
+  let _ = List.map pretty_print zones in
+    printf "%s\n" ( emit_chain ("table", "chain", List.map process_zone zones) )
+    
--- src/zone.ml
+++ src/zone.ml
@@ -6,10 +6,12 @@ let rec process_zone_data_as_source = function
   | Ip(a, b, m) as ip -> SourceAddress(ip_to_ip ip)
   | _ -> raise ImpossibleError
 
-let process_zone = function
-    Zone(name, nodes) -> let source_conditions = List.map process_zone_data_as_source nodes in
-                           source_conditions, MarkSourceZone(name)
-  | _ -> raise ImpossibleError
+let process_zone zone : (Ir.condition * bool) list * Ir.action = 
+  match zone with
+      Zone(name, nodes) -> let source_conditions = 
+        List.map ( fun node -> (process_zone_data_as_source node, false) ) nodes in
+        (source_conditions, MarkSourceZone(name))
+    | _ -> raise ImpossibleError
     
   
   
