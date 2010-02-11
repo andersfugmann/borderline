2802064e6c70371f42e6a52c5b78e8da50820895
afu <afu@71c2348b-e008-4d38-be3d-d357f6eb5cc4>
2010-02-11 20:52:51 +0000

    Add signature for ip6tables backend
    git-svn-id: https://svn.fugmann.net/borderline/trunk@152 71c2348b-e008-4d38-be3d-d357f6eb5cc4

---
+++ src/ip6tables.mli
@@ -0,0 +1,20 @@
+module StringMap: Map.S
+
+val zone_id : int ref
+val zone_map : int StringMap.t ref
+val elem : 'a list -> 'a
+val get_zone_id : StringMap.key -> int
+val gen_neg : bool -> string
+val choose_dir : string -> string -> Ir.direction -> string
+val get_state_name : Ir.statetype -> string
+val gen_zone_mask : Ir.direction -> StringMap.key * Ir.zone -> int * int
+val gen_zone_mask_str : Ir.direction -> StringMap.key * Ir.zone -> string
+val gen_condition : Ir.condition -> string * string
+val gen_conditions : string -> (Ir.condition * bool) list -> string
+val gen_action : Ir.action -> string
+val transform : Ir.chain Chain.Chain_map.t -> Ir.chain Chain.Chain_map.t
+val emit_rule : Ir.oper -> string
+val emit_rules : Ir.chain -> string list
+val filter : Ir.chain Chain.Chain_map.t -> Ir.chain Chain.Chain_map.t
+val create_chain : string list -> Ir.chain -> string list
+val emit_chains : Ir.chain Chain.Chain_map.t -> string list
